af3fc594e32d3c580dea9bc5d634b045fbbc5e10
Tony Chung <tonytony771111@gmail.com>
2022-08-11 11:02:28 +0800

    [Feature] 套件降版

--- packages/scrollable_positioned_list/lib/src/scrollable_positioned_list.dart
+++ packages/scrollable_positioned_list/lib/src/scrollable_positioned_list.dart
@@ -453,7 +453,7 @@ class _ScrollablePositionedListState extends State<ScrollablePositionedList>
     }
     if (_isTransitioning) {
       _stopScroll(canceled: true);
-      SchedulerBinding.instance.addPostFrameCallback((_) {
+      SchedulerBinding.instance?.addPostFrameCallback((_) {
         _startScroll(
           index: index,
           alignment: alignment,
@@ -500,7 +500,7 @@ class _ScrollablePositionedListState extends State<ScrollablePositionedList>
       final startCompleter = Completer<void>();
       final endCompleter = Completer<void>();
       startAnimationCallback = () {
-        SchedulerBinding.instance.addPostFrameCallback((_) {
+        SchedulerBinding.instance?.addPostFrameCallback((_) {
           startAnimationCallback = () {};
 
           opacity.parent = _opacityAnimation(opacityAnimationWeights).animate(
